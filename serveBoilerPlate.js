4d3191270075140638319e823424ee01a3cc252b
Garrett Griffey <garrettwgriffey@gmail.com>
2020-04-17 19:28:48 -0500

    Update serveBoilerPlate.js

--- serveBoilerPlate.js
+++ serveBoilerPlate.js
@@ -24,3 +24,22 @@ server.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+
+// ANOTHER EXAMPLE
+
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/htmlRoute")(app);
+require("./routes/apiRoute")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
